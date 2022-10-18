3de8958a3ef2e71feb2671d086445d2857663f46
Mmarvinn <cerkunik@gmail.com>
2022-10-18 12:21:28 +0300

    some fixes

---
+++ src/components/searchPanel/SearchNotFound.jsx
@@ -0,0 +1,11 @@
+export const SearchNotFound = () => {
+  return (
+    <div>
+      <h3>No Results Fount</h3>
+      <p>
+        We did not find any article that matches this search Make sure that the
+        search text is entered correctly Try using other search criteria
+      </p>
+    </div>
+  );
+};
